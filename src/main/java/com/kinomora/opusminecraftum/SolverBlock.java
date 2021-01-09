0a292a468b772b6923ba3a38c65320df60d07300
Kinomora <christmasallthetime@gmail.com>
2021-01-09 17:15:32 -0500

    Patchouli addins

---
+++ src/main/java/com/kinomora/opusminecraftum/SolverBlock.java
@@ -0,0 +1,9 @@
+package com.kinomora.opusminecraftum;
+
+import net.minecraft.block.BreakableBlock;
+
+public class SolverBlock extends BreakableBlock {
+    public SolverBlock(Properties properties) {
+        super(properties);
+    }
+}
